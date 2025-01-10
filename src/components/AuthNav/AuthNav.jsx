83b487fec64fc18b757cfe819dad45148f4d8cfa
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2025-01-10 12:21:08 +0200

    add RigistrationBtn

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,9 +1,12 @@
 import LogInBtn from "../LogInBtn/LogInBtn.jsx";
+import RegistrationBtn from "../RegistrationBtn/RegistrationBtn.jsx";
+import css from "./AuthNav.module.css";
 
 const AuthNav = () => {
   return (
-    <div>
+    <div className={css.authNav}>
       <LogInBtn />
+      <RegistrationBtn />
     </div>
   );
 };
---
+++ src/components/RegistrationBtn/RegistrationBtn.jsx
@@ -0,0 +1,11 @@
+import css from "./RegistrationBtn.module.css";
+
+const RegistrationBtn = () => {
+  return (
+    <button type="button" className={css.registrationBtn}>
+      Registration
+    </button>
+  );
+};
+
+export default RegistrationBtn;
