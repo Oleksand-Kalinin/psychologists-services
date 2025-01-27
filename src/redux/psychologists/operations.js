c038dc666e4d15ff0fdfbd28622154471c83b41e
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2025-01-27 12:46:51 +0200

    add sort on frontend

--- src/redux/psychologists/operations.js
+++ src/redux/psychologists/operations.js
@@ -21,6 +21,15 @@ export const startFetchPsychologists = createAsyncThunk(
                     id,
                     ...data,
                 }))
+                    .sort((a, b) => {
+                        if (payload === "A to Z") return a.name.localeCompare(b.name);
+                        if (payload === "Z to A") return b.name.localeCompare(a.name);
+                        if (payload === "Less than 10$") return a.price_per_hour - b.price_per_hour;
+                        if (payload === "Greater than 10$") return b.price_per_hour - a.price_per_hour;
+                        if (payload === "Popular") return b.rating - a.rating;
+                        if (payload === "Not popular") return a.rating - b.rating;
+                        return 0;
+                    })
                 : [];
 
             const totalPsychologistsCount = allResult.exists()
